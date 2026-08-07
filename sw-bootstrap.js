9d950d9534a2e753da3cefc92b5113f2c9ffe4a6
pureekangraw-ops <pureekangraw@gmail.com>
2026-08-07 21:46:57 +0700

    feat: load Metropolis R5 from bootstrap

--- sw-bootstrap.js
+++ sw-bootstrap.js
@@ -1,6 +1,25 @@
 "use strict";
 
 (() => {
+  function loadMetropolisR5() {
+    if (!document.querySelector('link[data-metropolis-r5="true"]')) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "metropolis-r5.css";
+      link.dataset.metropolisR5 = "true";
+      document.head.appendChild(link);
+    }
+    if (!document.querySelector('script[data-metropolis-r5="true"]')) {
+      const script = document.createElement("script");
+      script.src = "metropolis-r5.js";
+      script.async = false;
+      script.dataset.metropolisR5 = "true";
+      document.head.appendChild(script);
+    }
+  }
+
+  loadMetropolisR5();
+
   const supported = "serviceWorker" in navigator
     && (location.protocol === "https:"
       || ["localhost", "127.0.0.1"].includes(location.hostname));
